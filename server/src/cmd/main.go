d95ed776e7a3e715aa7365fb036e4141f8b4e668
mikaellafs <mikaellaferreira0@gmail.com>
2022-10-08 22:13:55 -0300

    Implement register user

--- server/src/cmd/main.go
+++ server/src/cmd/main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"contacttracing/src/clients"
 	"contacttracing/src/grpc/server"
+	"contacttracing/src/repositories"
 	"contacttracing/src/services"
+	"context"
+	"log"
 )
 
 func main() {
 	postgresDB := clients.NewPostgreSQLClient()
 	defer postgresDB.Close()
 
-	grpcService := services.NewGrpcService()
+	userRepo := repositories.NewPostGreSQLUserRepository(postgresDB)
+	err := userRepo.Migrate(context.TODO())
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	grpcService := services.NewGrpcService(userRepo)
 	s := server.NewGrpcCServer(grpcService)
 	s.Serve()
 }
--- server/src/services/grpc_service.go
+++ server/src/services/grpc_service.go
@@ -2,18 +2,66 @@ package services
 
 import (
 	"contacttracing/src/grpc/pb"
+	"contacttracing/src/interfaces"
+	"contacttracing/src/models/db"
+	"contacttracing/src/utils"
 	"context"
+	"log"
+	"net/http"
+	"time"
 )
 
-type GrpcService struct{}
+type GrpcService struct {
+	interfaces.UserRepository
+}
 
-func NewGrpcService() GrpcService {
-	return GrpcService{}
+func NewGrpcService(userRepo interfaces.UserRepository) GrpcService {
+	return GrpcService{userRepo}
 }
 
 func (s GrpcService) Register(ctx context.Context, request *pb.RegisterRequest) (*pb.RegisterResult, error) {
-	//TODO
-	return nil, nil
+	result := &pb.RegisterResult{
+		Status:   http.StatusOK,
+		ServerPk: "ok",
+	}
+
+	// Validate message
+	isValid, err := utils.ValidateMessage(request.GetRegister(), request.GetRegister().GetPk(), request.GetSignature())
+	if err != nil {
+		result.Status = http.StatusBadRequest
+		result.Message = "Failed to validate message: " + err.Error()
+		log.Println(result.Message)
+		return result, nil
+	}
+
+	if !isValid {
+		result.Status = http.StatusForbidden
+		result.Message = "Signature is not valid for this message"
+		log.Println(result.Message)
+		return result, nil
+	}
+
+	// Save new user
+	user := db.User{
+		UserId:   request.GetRegister().GetUserId(),
+		Pk:       request.GetRegister().GetPk(),
+		Password: request.GetRegister().GetPassword(),
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(150)*time.Millisecond)
+	defer cancel()
+
+	userSaved, err := s.Create(ctx, user)
+	if err != nil {
+		result.Status = http.StatusInternalServerError
+		result.Message = err.Error()
+		log.Println(result.Message)
+		return result, nil
+	}
+
+	log.Println(userSaved)
+
+	return result, nil
 }
 
 func (s GrpcService) ReportInfection(ctx context.Context, request *pb.ReportRequest) (*pb.ReportResult, error) {
