8d115a4131c40b4c2876136767b34dd8bad61fd2
mikaellafs <mikaellaferreira0@gmail.com>
2022-10-12 22:11:58 -0300

    Add cache for notification

--- server/src/interfaces/cacheRepository.go
+++ server/src/interfaces/cacheRepository.go
@@ -1,10 +1,14 @@
 package interfaces
 
 import (
+	"contacttracing/src/models/dto"
 	"time"
 )
 
 type CacheRepository interface {
 	SaveReport(userId, date string)
 	GetReportsFrom(userId string) []time.Time
+
+	SaveNotification(userId string, fromReport int64, date string)
+	GetNotificationFrom(userId string, reportId int64) *dto.Notification
 }
--- server/src/repositories/redisRepository.go
+++ server/src/repositories/redisRepository.go
@@ -1,7 +1,9 @@
 package repositories
 
 import (
+	"contacttracing/src/models/dto"
 	"log"
+	"strconv"
 	"time"
 
 	"github.com/go-redis/redis"
@@ -22,9 +24,10 @@ func NewRedisRepository(client *redis.Client) *RedisRepository {
 	return repo
 }
 
+// TODO: change date to time.Time and convert to string inside the function
 func (r *RedisRepository) SaveReport(userId, date string) {
 	log.Println(cacheRepositoryLog, "Save report from user ", userId, ", diagnosticated at ", date)
-	key := userId + "/" + date
+	key := r.makeReportKey(userId, date)
 	r.client.Set(key, date, 0)
 }
 
@@ -42,4 +45,41 @@ func (r *RedisRepository) GetReportsFrom(userId string) []time.Time {
 	}
 
 	return dates
+}
+
+// TODO: change date to time.Time and convert to string inside the function
+func (r *RedisRepository) SaveNotification(userId string, fromReport int64, date string) {
+	log.Println(cacheRepositoryLog, "Save notification for user", userId, "and report id", fromReport)
+	key := r.makeNotificationKey(userId, fromReport)
+	r.client.Set(key, date, 0)
+}
+
+func (r *RedisRepository) GetNotificationFrom(userId string, reportId int64) *dto.Notification {
+	log.Println(cacheRepositoryLog, "Get notification from user", userId, "and report id", reportId)
+
+	key := r.makeNotificationKey(userId, reportId)
+	result := r.client.Get(key)
+
+	if result == nil {
+		return nil
+	}
+
+	dateNotification, err := time.Parse(result.Val(), time.RFC3339)
+	if err != nil {
+		return nil
+	}
+
+	return &dto.Notification{
+		ReportId:     reportId,
+		ForUser:      userId,
+		DateNotified: dateNotification,
+	}
+}
+
+func (r *RedisRepository) makeReportKey(userId, reportDate string) string {
+	return userId + "/" + reportDate
+}
+
+func (r *RedisRepository) makeNotificationKey(userId string, reportId int64) string {
+	return userId + "/" + strconv.FormatInt(reportId, 10)
 }
