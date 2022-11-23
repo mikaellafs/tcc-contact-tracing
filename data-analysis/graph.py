d26b24867aadfb1a0bc91bef0b49445c23d5f0e4
mikaellafs <mikaellaferreira0@gmail.com>
2022-11-22 23:43:40 -0300

    Draw weighted graph

--- data-analysis/graph.py
+++ data-analysis/graph.py
@@ -1,34 +1,75 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def build_graph_from_df(data, isMulti=False):
+min_duration_risky=15*60 # 15 minutes
+min_distance_risky=200 # 2 meters
+
+# TODO: CONSIDER BOTH DIRECTIONS OR GET THE GREATEST ONE
+def build_graph_from_df(data, weight_name, args, isMulti=False):
     """ Build graph from dataframe (user1, user2, weight)"""
-    G =  nx.Graph()
+    if isMulti:
+        G = nx.MultiDiGraph()
+    else:
+        G = nx.Graph()
 
     for _, contact in data.iterrows():
         if not isMulti and (G.has_edge(contact["user1"], contact["user2"]) or G.has_edge(contact["user2"], contact["user1"])):
             continue
-
-        G.add_edge(contact["user1"], contact["user2"], contact["weight"])
+        
+        G.add_edge(contact["user1"], contact["user2"], weight=contact[weight_name], data=contact[args].to_dict())
     
     return G
 
-def draw(G, labels=False, filename='graph.png'):
+def draw(G, labels=False, filename='graph5.png'):
     """ Draw a graph """
     # Compute position of nodes
-    # pos = nx.spring_layout(G, k=0.5)
-    pos = nx.kamada_kawai_layout(G)
+    pos = nx.spring_layout(G, k=10, seed=5)
 
     # Draw nodes and edges
+    if labels:
+        nx.draw_networkx_labels(G, pos)
+
     nx.draw_networkx_nodes(G, pos)
     nx.draw_networkx_edges(
         G, pos,
-        # connectionstyle='arc3, rad=0.2'
+        connectionstyle='arc3, rad=0.2'
     )
 
     plt.savefig(make_file_path(filename))
     plt.show()
 
+def draw_with_weights(G, elarge, esmall, labels=False, filename='wgraph.png'):
+    pos = nx.spring_layout(G, k=10, seed=5)
+
+    # Draw nodes
+    if labels:
+        nx.draw_networkx_labels(G, pos)
+
+    nx.draw_networkx_nodes(G, pos)
+
+    # Draw edges with weights
+    nx.draw_networkx_edges(G, pos, edgelist=elarge, width=2)
+    nx.draw_networkx_edges(
+    G, pos, edgelist=esmall, width=2, alpha=0.5, edge_color="b", style="dashed"
+    )
+
+    plt.savefig(make_file_path(filename))
+    plt.show()
+
+def split_edge_sizes(G, operator, criteria):
+    elarge = [(u, v) for (u, v, d) in G.edges(data=True) if operator(d["weight"], criteria)]
+    esmall = [(u, v) for (u, v, d) in G.edges(data=True) if not operator(d["weight"], criteria)]
+
+    return elarge, esmall
+
+def split_contact_risks(G, distance, duration):
+    elarge = [(u, v) for (u, v, d) in G.edges(data=True) if is_at_risk(d['data'][distance], d['data'][duration])]
+    esmall = [(u, v) for (u, v, d) in G.edges(data=True) if not is_at_risk(d['data'][distance], d['data'][duration])]
+
+    return elarge, esmall
+
+def is_at_risk(distance, duration):
+    return distance <=min_distance_risky and duration >= min_duration_risky
+
 def make_file_path(filename):
-    return 'generated/' + filename
-    
+    return 'generated/graph/' + filename
--- data-analysis/main.py
+++ data-analysis/main.py
@@ -1,6 +1,7 @@
 import db
 import graph
 import processing
+import operator
 
 import pandas as pd
 
@@ -21,9 +22,27 @@ def get_from_db():
 def get_from_csv(filepath):
     return pd.read_csv(filepath)
 
-
-df = get_from_csv("data/data-1668650971716.csv")
+# Process data
+df = get_from_csv("data/data-1669162316639.csv")
 df = processing.parse_timestamp_types(df)
-processing.aggregate_constant_contacts(df)
-    
+
+constant_df = processing.aggregate_constant_contacts(df)
+metrics_df = processing.get_contact_metrics(constant_df)
+
+# Draw graphs
+metrics = metrics_df.reset_index()
+
+G = graph.build_graph_from_df(metrics, "avg_distance")
+graph.draw_with_weights(G, *graph.split_edge_sizes(G, operator.le, 200), filename='wgraph-distance-avg.png')
+
+
+G = graph.build_graph_from_df(metrics, "max_duration")
+graph.draw_with_weights(G, *graph.split_edge_sizes(G, operator.ge, 60*15), filename='wgraph-duration-max.png')
+
+data_risk_columns = ["avg_distance", "max_duration"]
+G = graph.build_graph_from_df(metrics, "avg_distance", data_risk_columns)
+
+at_risk_nodes, not_risk_nodes = graph.split_contact_risks(G, *data_risk_columns)
+graph.draw_with_weights(G, at_risk_nodes, not_risk_nodes, filename='wgraph-risk.png')
+graph.draw_with_weights(G, at_risk_nodes, not_risk_nodes, filename='wgraph-risk-labeled.png', labels=True)
 
